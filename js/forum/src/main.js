13a2b8c5948507a2ac1ece8f827e0a80cc6b509a
ryanvade <ryanvader82@gmail.com>
2016-06-30 16:39:34 -0500

    Added README.md and .gitignore

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -2,6 +2,7 @@ import { extend } from 'flarum/extend';
 import UserCard from 'flarum/components/UserCard'
 
 app.initializers.add('ryanvade-flarum-ext-birthdays', function() {
+  console.log("Inside of main.js for flarum-ext-birthdays");
   extend(UserCard.prototype, 'infoItems', function(items){
     items.add('Birthday', <div>BIRTHDAY %%USER_BIRTHDAY%%</div>);
   });
